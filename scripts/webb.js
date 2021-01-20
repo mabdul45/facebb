6f6d50c9a74a46ef5c2bf2f43613f3f309e0a733
MABDUL <moh.abdullahi2003@gmail.com>
2021-01-20 03:00:29 +0100

    finished learn to code

---
+++ scripts/webb.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var X = document.getElementById("mylinks");
+  if (X.style.display === "block") {
+    X.style.display = "none";
+  } else {
+    X.style.display = "block";
+  }
+}
